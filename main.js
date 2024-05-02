dbca74e708af3da88c31ca53a7e2aeda483c3231
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-02 14:12:19 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,21 @@
+// let sdutens =["shamsiddin","ali","zidan","nodir","soloh","hasan" ]
+// console.log(sdu);
+// sdutens.push("shamsi")
+
+// console.log(sdutens);
+
+// sdutens.unshift("usmon")
+// console.log(sdutens);
+// sdutens.pop
+// sdutens.shift
+
+const arNAME = prompt["olma","banan","gilos","qulupnay"];
+let l = arNAME,length;
+let t
+alert(l)
+let d = confirm("siz arraydan element ochirmoqchimisiz")
+if (d) {
+    t = arNAME.pop() ;
+}
+alert(t)
+alert(arNAME)
