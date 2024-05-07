63fa7439a3ebcafb1a791c431c509c172e8c2d0b
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-07 14:36:06 +0500

    {.}

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-let array = prompt["banan","qulupnay","olma","tarvuz"]
-let long = array.lenght
-console.log("Arrayning uzunlig 4 = " + long);
-alert("Arrayning uzunlig 4 = " + long);
+const array = ["banan","qulupnay","olma","tarvuz"]
+let long = array.lenght;
+console.log("Arrayning uzunlig 1 " + long + " teng");
+alert("Arrayning uzunlig " + long + " teng");
 
 let result = confirm("Siz jarayon etishni hohlaysizmi")
 
 if (result) {
     array.pop()
-    let l = array.length
+    let l = array.lenght;
     alert("Kesilgan arraynin uzunligi = 1 " + l)
     console.log("Kesilgan arraynin uzunligi = 1 " + l);
     
+    
 }else{
     let l = array.lenght;
+    alert("siz jarrayon toxtatganiz uchun l" + l )
     console.log("siz jarrayon toxtatganiz uchun l" + l );
 }
